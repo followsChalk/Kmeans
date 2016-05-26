199b4d3c08f4a85cbcb81a2f0572f9a4e97eb3fc
Vito Canadi <vito.canadi@gmail.com>
2016-05-26 21:19:40 +0200

    display iterations

--- Kmeans.hs
+++ Kmeans.hs
@@ -5,6 +5,9 @@ import Data.List
 import Data.Function (on)
 
 import Point
+import System.Random
+import System.Random.Shuffle
+
 import UniformList
 
 
@@ -14,35 +17,51 @@ import UniformList
 
 n=30
 m=20
+iters =6
 
-
-examples :: [[Point Float]]
-examples =  [
-                [ [2,5], [1,1],[1,5],[10,10],[15,15],[10,15]],  
-                [ [fromIntegral x,fromIntegral y] | x<-[0..n-1],y<-[0..m-1], x^2+y^2<200 ],
-                [ [fromIntegral x,fromIntegral y] | x<-[0..n-1],y<-[0..m-1], simplexOr x y  ]
-            ]
+examples  :: [[Point Float]]
+examples  =     [
+                 [ [2,5], [1,1],[1,5],[10,10],[15,15],[10,15]],
+                 [ [fromIntegral x,fromIntegral y] | x<-[0..n-1],y<-[0..m-1], x^2+y^2<60 ],
+                 [ [fromIntegral x,fromIntegral y] | x<-[0..n-1],y<-[0..m-1], simplexOr x y  ]
+                ]
 
 simplexOr :: Int -> Int -> Bool
 simplexOr x y = or [
                         x>3 && x<=8 && y>4 && y<=7,
                         x>15 && x<=17 && y>4 && y<=7,
                         x+2*y-45>0 && x+2*y-55<0 && x-y-5>0 && x-y-10<=0,
-
                         x+2*y-40>0 && x+2*y-50<0 && x-y+5>0 && x-y<=0
-
                     ]
-main :: IO()
-main = do
-        putStrLn $  concat $
+
+showMultipleExamples gen =
+                    concat $
                     map (\(i,example) ->
                             concat $
                             map (\k ->
-                                    "example " ++ show i ++ "(k="++show k++")\n" ++
-                                    displayLabeled n m  (zip example (kmeans k example))
+                                    "example " ++ show i ++ ", iteration "++show iters++"(k="++show k++")\n" ++
+                                    let example' = shuffle' example (length example) gen  
+                                    in displayLabeled n m  (zip example' (kmeans iters k example'))
                                 ) [2,3,4]
                         )      
                         $zip [1..] examples
+
+
+showMultipleIterations example =
+                    concat $
+                    ["multiple itarations on one example\n"]++
+                    map (\iter ->
+                            "example 2, iteration " ++ show iter ++ "(k=4)\n" ++
+                            displayLabeled n m  (zip example (kmeans iter 4 example)) 
+                        )
+                        [0..iters]
+main :: IO()
+main = do
+        gen<-newStdGen
+        let example=examples!!2;
+        --putStrLn $ showMultipleIterations $ shuffle' example (length example) gen 
+        putStrLn $ showMultipleExamples gen
+        
 
 
 ----Definition-------------------------------------------
@@ -55,9 +74,8 @@ means xs cs = map meanOverFsts $  groupBy ((==) `on` snd) $   sortBy (compare `o
 
 
 
-kmeans :: Int ->[Point Float] -> [Int]
-kmeans k xs = let kmeansSteps = 10
-            in iteration kmeansSteps $ uniformList k (length xs)
+kmeans :: Int -> Int ->[Point Float] -> [Int]
+kmeans iters k xs = iteration iters $ uniformList k (length xs)
     where 
     meansXs         =   means xs
     iteration 0 cs  =   cs
